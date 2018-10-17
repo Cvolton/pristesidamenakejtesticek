fe7226a1efdd57740340443143f562d50ca0c9b3
Michael Brabec <brabemi@LAB222-05.UZLABINA>
2018-10-17 08:54:50 +0200

    Pridan vektor

---
+++ 01-Vector/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vector vec = new Vector();
+			vec.x = 2;
+			vec.y = 3;
+			vec.Vypis();
+
+			Vector nevec = new Vector();
+			nevec.x = -69;
+			nevec.y = 7;
+			nevec.Vypis();
+
+			Vector s = vec.Secti(nevec);
+			double sc = vec.Uhel(nevec);
+			Console.WriteLine(sc);
+		}
+	}
+}
---
+++ 01-Vector/Vector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+	class Vector
+	{
+		public double x, y;
+
+		public Vector(double x = 0, double y = 0) //konstruktor
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public void Vypis()
+		{
+			Console.WriteLine("({0}, {1})", x, y);
+		}
+
+		public Vector Secti(Vector vec)
+		{
+			return new Vector(x + vec.x, y + vec.y);
+		}
+
+		public double Soucin(Vector vec)
+		{
+			return (this.x * vec.x) + (this.y) * (vec.y);
+		}
+
+		public double Uhel(Vector vec)
+		{
+			return Math.Acos((this.x * vec.x + this.y * vec.y) / (Math.Sqrt(Math.Pow(this.x, 2) + Math.Pow(this.y, 2)) * Math.Pow(vec.x, 2) + Math.Pow(vec.y, 2)));
+		}
+	}
+}
